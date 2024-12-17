6215c6ee45fe76160a2c6826095b489dbf259742
MeeedAymen <mohamedaymenazza@gmail.com>
2024-12-17 11:33:11 +0100

    auth version final

--- frontend/src/app/component/auth/auth.service.ts
+++ frontend/src/app/component/auth/auth.service.ts
@@ -3,54 +3,55 @@ import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
 
 export interface RegisterRequest {
-  name: string;
-  email: string;
-  password: string;
-  phoneNumber: string;
+    name: string;
+    email: string;
+    password: string;
+    phoneNumber: string;
 }
 
 interface LoginResponse {
-  token: string;
-  userDetails: {
-    id: number;
-    name: string;
-    email: string;
-    phoneNumber: string;
-    role: string;
-    createdAt: string;
-    updatedAt: string;
-    active: boolean;
-  };
+    token: string;
+    userDetails: {
+        id: number;
+        name: string;
+        email: string;
+        phoneNumber: string;
+        role: string;
+        createdAt: string;
+        updatedAt: string;
+        active: boolean;
+    };
 }
 
 @Injectable({
-  providedIn: 'root',
+    providedIn: 'root',
 })
 export class AuthService {
-  private apiUrl = 'environment.apiUrl';  // Get the apiUrl from environment.ts
+    private apiUrl = 'http://localhost:8080/api/authentication';
+    //private apiUrl = 'environment.apiUrl';  // Get the apiUrl from environment.ts
 
-  constructor(private http: HttpClient) {}
+    constructor(private http: HttpClient) {}
 
-  login(email: string, password: string): Observable<LoginResponse> {
-    const payload = { email, password };
-    return this.http.post<LoginResponse>(`${this.apiUrl}/login`, payload);
-  }
+    login(email: string, password: string): Observable<LoginResponse> {
+        const payload = { email, password };
+        return this.http.post<LoginResponse>(`${this.apiUrl}/login`, payload);
+    }
 
-  register(registerData: RegisterRequest): Observable<any> {
-    return this.http.post(`${this.apiUrl}/register`, registerData);
-  }
+    register(registerData: RegisterRequest): Observable<any> {
+        return this.http.post(`${this.apiUrl}/register`, registerData);
+    }
 
-  logout() {
-    localStorage.removeItem('token');
-    localStorage.removeItem('user');
-  }
+    logout() {
+        localStorage.removeItem('token');
+        localStorage.removeItem('user');
+    }
 
-  isLoggedIn(): boolean {
-    return !!localStorage.getItem('token');
-  }
+    isLoggedIn(): boolean {
+        return !!localStorage.getItem('token');
+    }
 
-  getCurrentUser() {
-    const userJson = localStorage.getItem('user');
-    return userJson ? JSON.parse(userJson) : null;
-  }
+    getCurrentUser() {
+        const userJson = localStorage.getItem('user');
+        return userJson ? JSON.parse(userJson) : null;
+    }
 }
