a75adfc483441d5a8bc2e42cc0f5cb88ec711270
MeeedAymen <mohamedaymenazza@gmail.com>
2024-12-18 09:05:22 +0100

    frontend components setup

---
+++ frontend/src/app/component/pack/pack.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-pack',
+  imports: [],
+  templateUrl: './pack.component.html',
+  styleUrl: './pack.component.css'
+})
+export class PackComponent {
+
+}
---
+++ frontend/src/app/component/vente/vente.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-vente',
+  imports: [],
+  templateUrl: './vente.component.html',
+  styleUrl: './vente.component.css'
+})
+export class VenteComponent {
+
+}
