201b73bcd3f4663599afa0a3cebf513cb76abfa5
mohamed-aymen-ibk <a.azza@ibk.ma>
2024-12-27 12:31:14 +0100

    responsive

--- frontend/src/app/component/shared/navbar/navbar.component.ts
+++ frontend/src/app/component/shared/navbar/navbar.component.ts
@@ -6,28 +6,33 @@ import {NgIf} from "@angular/common";
 @Component({
   selector: 'app-navbar',
   templateUrl: './navbar.component.html',
+  styleUrls: ['./navbar.component.css'],
   imports: [
-    NgIf,
-    RouterLink
-  ],
-  styleUrls: ['./navbar.component.css']
+    RouterLink,
+    NgIf
+  ]
 })
 export class NavbarComponent {
-  isLogoutModalVisible = false; // Track modal visibility
+  isLogoutModalVisible = false;
+  isMenuOpen = false; // Track menu visibility for mobile
 
   constructor(private authService: AuthService, private router: Router) {}
 
   openLogoutModal(): void {
-    this.isLogoutModalVisible = true; // Show the modal
+    this.isLogoutModalVisible = true;
   }
 
   closeLogoutModal(): void {
-    this.isLogoutModalVisible = false; // Hide the modal
+    this.isLogoutModalVisible = false;
   }
 
   confirmLogout(): void {
-    this.isLogoutModalVisible = false; // Hide the modal
+    this.isLogoutModalVisible = false;
     this.authService.logout();
     this.router.navigate(['/auth/login']);
+  }
+
+  toggleMenu(): void {
+    this.isMenuOpen = !this.isMenuOpen;
   }
 }
